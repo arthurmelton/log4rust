d704a8fb8a1de302331e6e875d72108a4c4a805c
arthurmelton <amtitan@icloud.com>
2022-09-15 12:37:29 -0400

    use log4rust in web

--- examples/web/src/main.rs
+++ examples/web/src/main.rs
@@ -5,27 +5,27 @@ fn main() -> Result<(), std::io::Error> {
         .time(Time::Local)
         .set_type(Log::Info)?.web(
             "{\"Type\":\"Info\",\"Body\":\"{}\"}",
-            ureq::post("https://Loggerz.free.beeceptor.com")
+            ureq::post("https://log4rust.free.beeceptor.com")
                 .set("Content-Type", "application/json"),
         )?
         .set_type(Log::Warn)?.web(
             "{\"Type\":\"Warn\",\"Body\":\"{}\"}",
-            ureq::post("https://Loggerz.free.beeceptor.com")
+            ureq::post("https://log4rust.free.beeceptor.com")
                 .set("Content-Type", "application/json"),
         )?
         .set_type(Log::Error)?.web(
             "{\"Type\":\"Error\",\"Body\":\"{}\"}",
-            ureq::post("https://Loggerz.free.beeceptor.com")
+            ureq::post("https://log4rust.free.beeceptor.com")
                 .set("Content-Type", "application/json"),
         )?
         .set_type(Log::Fatal)?.web(
             "{\"Type\":\"Fatal\",\"Body\":\"{}\"}",
-            ureq::post("https://Loggerz.free.beeceptor.com")
+            ureq::post("https://log4rust.free.beeceptor.com")
                 .set("Content-Type", "application/json"),
         )?
         .save()
         .unwrap();
-    // go to https://beeceptor.com/console/loggerz to see this happen
+    // go to https://beeceptor.com/console/log4rust to see this happen
     info!("This is some info");
     warn!("This is a warning");
     error!("This is an error");
