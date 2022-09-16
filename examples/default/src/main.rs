8fa8d6b05ddcf0776231993065560d81de275a8c
arthurmelton <amtitan@icloud.com>
2022-09-16 17:05:11 -0400

    default is easier to read

--- examples/default/src/main.rs
+++ examples/default/src/main.rs
@@ -3,10 +3,22 @@ use log4rust::*;
 fn main() -> Result<(), std::io::Error> {
     log4rust::new()
         .time(Time::Local)
-        .set_type(Log::Info)?.color(Color::TrueColor{r:0,g:255,b:255})?.console(Console::Stdout)?.backtrace(Backtrace::_None)?
-        .set_type(Log::Warn)?.color(Color::TrueColor{r:255,g:215,b:185})?.console(Console::Stderr)?.backtrace(Backtrace::_None)?
-        .set_type(Log::Error)?.color(Color::TrueColor{r:255,g:100,b:0})?.console(Console::Stderr)?.backtrace(Backtrace::Simple)?
-        .set_type(Log::Fatal)?.color(Color::TrueColor{r:255,g:0,b:0})?.console(Console::Stderr)?.backtrace(Backtrace::Complex)?
+        .set_type(Log::Info)?
+            .color(Color::TrueColor{r:0,g:255,b:255})?
+            .console(Console::Stdout)?
+            .backtrace(Backtrace::_None)?
+        .set_type(Log::Warn)?
+            .color(Color::TrueColor{r:255,g:215,b:185})?
+            .console(Console::Stderr)?
+            .backtrace(Backtrace::_None)?
+        .set_type(Log::Error)?
+            .color(Color::TrueColor{r:255,g:100,b:0})?
+            .console(Console::Stderr)?
+            .backtrace(Backtrace::Simple)?
+        .set_type(Log::Fatal)?
+            .color(Color::TrueColor{r:255,g:0,b:0})?
+            .console(Console::Stderr)?
+            .backtrace(Backtrace::Complex)?
         .save()
         .unwrap();
     info!("This is some info");
